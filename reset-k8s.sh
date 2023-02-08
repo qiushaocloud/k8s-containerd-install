243720612147c1f00338f1264075f9ebbae40892
qiushaocloud <qiushaocloud@126.com>
2023-02-09 00:28:41 +0800

    change download file

--- reset-k8s.sh
+++ reset-k8s.sh
@@ -1,3 +1,8 @@
-kubeadm reset -f --kubeconfig $PWD/kubeadm-config.yml
-#kubeadm reset -f --cri-socket unix:///run/containerd/containerd.sock 
+if [ -f "kubeadm-config.yml" ]; then
+    echo "kubeadm reset -f --kubeconfig $PWD/kubeadm-config.yml"
+    kubeadm reset -f --kubeconfig $PWD/kubeadm-config.yml
+else
+    echo "kubeadm reset -f --cri-socket unix:///run/containerd/containerd.sock"
+    kubeadm reset -f --cri-socket unix:///run/containerd/containerd.sock 
+fi
 
