e916ccc6b0362b92881bc881bf2ba87117ed1e7c
qiushaocloud <qiushaocloud@126.com>
2023-02-19 00:08:14 +0800

    fix reset k8s sh

--- cp-k8s-node-need-files.sh
+++ cp-k8s-node-need-files.sh
@@ -1,7 +1,7 @@
 if [ -f "cri-containerd-cni-1.6.16-linux-amd64.tar.gz" ]; then
-    echo "tar -zcvf install-k8s.sh k8s-node-need-files.tgz cni-plugins-linux-amd64-v1.2.0.tgz libseccomp-2.5.1-1.el8.x86_64.rpm cri-containerd-cni-1.6.16-linux-amd64.tar.gz install-k8s.sh reset-k8s.sh"
+    echo "tar -zcvf k8s-node-need-files.tgz cni-plugins-linux-amd64-v1.2.0.tgz libseccomp-2.5.1-1.el8.x86_64.rpm cri-containerd-cni-1.6.16-linux-amd64.tar.gz install-k8s.sh reset-k8s.sh"
     tar -zcvf k8s-node-need-files.tgz cni-plugins-linux-amd64-v1.2.0.tgz libseccomp-2.5.1-1.el8.x86_64.rpm cri-containerd-cni-1.6.16-linux-amd64.tar.gz install-k8s.sh reset-k8s.sh
 else
-    echo "tar -zcvf install-k8s.sh k8s-node-need-files.tgz cni-plugins-linux-amd64-v1.2.0.tgz libseccomp-2.5.1-1.el8.x86_64.rpm install-k8s.sh reset-k8s.sh"
+    echo "tar -zcvf k8s-node-need-files.tgz cni-plugins-linux-amd64-v1.2.0.tgz libseccomp-2.5.1-1.el8.x86_64.rpm install-k8s.sh reset-k8s.sh"
     tar -zcvf k8s-node-need-files.tgz cni-plugins-linux-amd64-v1.2.0.tgz libseccomp-2.5.1-1.el8.x86_64.rpm install-k8s.sh reset-k8s.sh
 fi
