238d266dea40d555dad1253ab81d5e4f3cb1285f
qiushaocloud <qiushaocloud@126.com>
2023-02-19 01:15:46 +0800

    change kube-flannel

--- reset-k8s.sh
+++ reset-k8s.sh
@@ -1,14 +1,14 @@
-crictl ps -a|grep Exited|awk '{print $1}'|(xargs crictl rm -f || true)
+# crictl ps -a|grep Exited|awk '{print $1}'|(xargs crictl rm -f || true)
 
-if [ -f "kubeadm-config.yml" ]; then
-    echo "kubeadm reset -f --kubeconfig $PWD/kubeadm-config.yml --cri-socket unix:///run/containerd/containerd.sock"
-    kubeadm reset -f --kubeconfig $PWD/kubeadm-config.yml --cri-socket unix:///run/containerd/containerd.sock
-else
-    echo "kubeadm reset -f --cri-socket unix:///run/containerd/containerd.sock"
-    kubeadm reset -f --cri-socket unix:///run/containerd/containerd.sock 
-fi
+# if [ -f "kubeadm-config.yml" ]; then
+    # echo "kubeadm reset -f --kubeconfig $PWD/kubeadm-config.yml --cri-socket unix:///run/containerd/containerd.sock"
+    # kubeadm reset -f --kubeconfig $PWD/kubeadm-config.yml --cri-socket unix:///run/containerd/containerd.sock
+# else
+echo "kubeadm reset -f --cri-socket unix:///run/containerd/containerd.sock"
+kubeadm reset -f --cri-socket unix:///run/containerd/containerd.sock 
+# fi
 
-rm -rf /etc/cni/net.d
+# rm -rf /etc/cni/net.d
 ipvsadm --clear
 rm -rf ~/.kube
 rm -rf /etc/kubernetes/manifests
