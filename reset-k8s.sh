90a1d2ce99a2a0fd3f81c0ccea28de900c84f766
qiushaocloud <qiushaocloud@126.com>
2023-02-19 00:00:22 +0800

    fix reset k8s sh

--- reset-k8s.sh
+++ reset-k8s.sh
@@ -1,4 +1,6 @@
 crictl ps -a|grep Exited|awk '{print $1}'|(xargs crictl rm -f || true)
+rm -rf /etc/cni/net.d
+ipvsadm --clear
 
 if [ -f "kubeadm-config.yml" ]; then
     echo "kubeadm reset -f --kubeconfig $PWD/kubeadm-config.yml --cri-socket unix:///run/containerd/containerd.sock"
